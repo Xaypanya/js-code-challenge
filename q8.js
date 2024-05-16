3bf901de2b7605f81a6b15500abddb922dd5b7a8
Xaypanya Phongsa <dackogan@gmail.com>
2024-05-17 01:09:31 +0700

    add question txt

---
+++ q8.js
@@ -0,0 +1,9 @@
+// Create a function that will convert from Celsius to Fahrenheit
+
+function CelToFah(celNum){
+    return celNum * 9 / 5 + 32
+}
+
+const Ctemp = 30
+
+console.log(CelToFah(Ctemp)) // output = 86
---
+++ q9.js
@@ -0,0 +1,9 @@
+// Create a function that will convert from Fahrenheit to Celsius
+
+function FahToCel(fNum){
+    return 5 * (fNum - 32) / 9
+}
+
+const Ftemp = 86
+
+console.log(FahToCel(Ftemp)) //output = 30
